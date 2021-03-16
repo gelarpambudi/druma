8e60d8839c941b2390699c10da68761d0da5a4b3
gelar <gelargelur@gmail.com>
2021-03-16 20:47:36 +0700

    change url with hostname

--- req.py
+++ req.py
@@ -7,8 +7,8 @@ from image_processing import save_image
 
 ALLOWED_EXTENSIONS = set(['tiff', 'tif', 'jpg', 'jpeg', 'png', 'JPG', 'JPEG', 'PNG', 'TIF', 'TIFF'])
 URL = [
-        'http://localhost:5555/api/predict-deepforest',
-        'http://localhost:4444/api/hough-transform'
+        'http://deepforest.druma.com:5555/api/predict-deepforest',
+        'http://hough.druma.com:4444/api/hough-transform'
         ]
 
 
