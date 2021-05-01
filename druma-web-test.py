d71903c84d32ea327546d79c2c073e5a56888c2e
Gelar Pambudi <gelargelur@gmail.com>
2021-05-01 20:30:23 +0700

    change port to 80

--- druma-web-test.py
+++ druma-web-test.py
@@ -123,4 +123,4 @@ def predict():
         return render_template("predict.html")
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port='7777')
+    app.run(debug=True, host='0.0.0.0', port='80')
